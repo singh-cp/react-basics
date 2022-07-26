af42d603d486c364a9d3ea18453054b3a8aca68b
singh-cp <chandraprakash.iam@gmail.com>
2022-07-26 15:49:52 +0530

    static project-1

---
+++ free-codecamp-react project-1/src/App.js
@@ -0,0 +1,13 @@
+import "./App.css";
+import Main from "./components/Main";
+import Navbar from "./components/Navbar";
+function App() {
+  return (
+    <div className="container">
+      <Navbar />
+      <Main />
+    </div>
+  );
+}
+
+export default App;
---
+++ free-codecamp-react project-1/src/components/Navbar.js
@@ -0,0 +1,19 @@
+import React from "react";
+
+export default function Navbar() {
+  return (
+    <div>
+      <nav className="navbar">
+        <div className="navbar-logo">
+          <img
+            className="logo"
+            src="https://cdn4.iconfinder.com/data/icons/logos-3/600/React.js_logo-512.png"
+            alt=""
+          />
+          <p>ReactFacts</p>
+        </div>
+        <p>React Course Project-1</p>
+      </nav>
+    </div>
+  );
+}
